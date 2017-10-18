74ab1b86eac81b96b3e15d59118ec4015e3c816d
Marcelo Aruzamen <marcelo@apptegy.com>
2017-10-18 17:40:36 -0400

    Added Material module and removed default content from app component html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginModule } from './login/login.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoginModule
+    LoginModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
